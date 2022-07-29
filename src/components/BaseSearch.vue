5e0f4cd730835b32d24c12e119c2f6bf67dfab51
marcocalligaris <marcocally@gmail.com>
2022-07-29 15:41:15 +0200

    Created BaseSearch

---
+++ src/components/BaseSearch.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <input 
+        type="text" 
+        v-model.trim="searchTerm"
+        @keyup.enter="emitSearch"
+        :placeholder="placeholder" 
+        />
+        <button 
+        type="button" 
+        @click="emitSearch">Cerca</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BaseSearch',
+    props: {
+        placeholder: String,
+    },
+    data() {
+        return {
+            searchTerm: '',
+        }
+    },
+    methods: {
+        emitSearch() {
+            this.$emit('search', this.searchTerm);
+        }
+    },
+}
+</script>
+
+<style></style>
