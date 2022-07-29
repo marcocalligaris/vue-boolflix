3af2344813d1e8aa88f70fc93a266e41713103d9
marcocalligaris <marcocally@gmail.com>
2022-07-29 15:59:36 +0200

    Added BaseHeader

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <BaseSearch placeholder="Cerca qui" @search="fetch" />
+    </div>
+</template>
+
+<script>
+import BaseSearch from './BaseSearch.vue';
+
+export default {
+    name: "BaseHeader",
+    components: { BaseSearch },
+    methods: {
+        fetch(query) {
+            console.log(query);
+        }
+    },
+}
+</script>
+
