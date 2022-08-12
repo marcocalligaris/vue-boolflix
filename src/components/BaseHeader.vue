e24dc265fedd66381e0b1920c67244ff6fdb037c
marcocalligaris <marcocally@gmail.com>
2022-08-12 14:15:44 +0200

    Fixed errors

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,20 +1,13 @@
 <template>
     <div>
-        <BaseSearch placeholder="Cerca qui" @search="fetch" />
     </div>
 </template>
 
 <script>
-import BaseSearch from './BaseSearch.vue';
 
 export default {
     name: "BaseHeader",
-    components: { BaseSearch },
-    methods: {
-        fetch(query) {
-            console.log(query);
-        }
-    },
+    
 }
 </script>
 
