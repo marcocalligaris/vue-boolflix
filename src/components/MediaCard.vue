0cdc660523d99ef97caf76540cdb81bc1bbdb0bd
marcocalligaris <marcocally@gmail.com>
2022-08-12 14:15:12 +0200

    Added Media Card

---
+++ src/components/MediaCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <ul>
+        <li>{{ media.title || media.name }}</li>
+        <li>{{ media.original_title || media.original_name }}</li>
+        <li>
+            <img v-if="hasFlag" :src="flagSrc" :alt="media.original_language">
+            <span v-else>{{ media.original_language }}</span>
+        </li>
+        <li>{{ media.vote_average }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+name: 'MediaCard',
+props: {
+    media: Object,
+},
+computed: {
+    hasFlag() {
+        const flags = ['it', 'en'];
+        return flags.includes(this.media.original_language);
+    },
+    flagSrc() {
+        return require(`../assets/img/flags/${this.media.original_language}.png`);
+    }
+},
+}
+</script>
+
+<style scoped>
+    img {
+        width: 50px;
+    }
+</style>
