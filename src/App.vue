4e6c305124134fb06187c609724fe32aae9baa60
marcocalligaris <marcocally@gmail.com>
2022-08-12 14:16:23 +0200

    Milestone 2

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <BaseSearch placeholder="Cerca qui" @search="fetch"/>
+    <BaseSearch placeholder="Cerca qui" @search="search" />
     <section id="movies">
       <h1>MOVIES</h1>
-      <ul v-for="movie in movies" :key="movie.id">
-        <li>{{movie.title}}</li>
-        <li>{{movie.original_title}}</li>
-        <li>{{movie.original_language}}</li>
-        <li>{{movie.vote_average}}</li>
-      </ul>
+      <MediaCard v-for="movie in movies" :key="movie.id" :media='movie' />
+
+    </section>
+    <section id="series">
+      <h1>SERIES</h1>
+      <MediaCard v-for="serie in series" :key="serie.id" :media='serie' />
     </section>
   </div>
 </template>
@@ -16,12 +16,14 @@
 <script>
 import BaseSearch from './components/BaseSearch.vue';
 import axios from 'axios';
+import MediaCard from './components/MediaCard.vue';
 export default {
     name: "App",
-    components: { BaseSearch },
+    components: { BaseSearch, MediaCard, },
     data() {
       return {
         movies: [],
+        series: [],
         api: {
           baseUri: 'https://api.themoviedb.org/3',
           key: 'b6a442a03c6d2f2cb8e2c9395b576193',
@@ -30,7 +32,17 @@ export default {
       };
     },
     methods: {
-        fetch(query) {
+        search(query) {
+          if(!query) {this.movies = this.series = [];
+          return;
+          }
+
+          this.getData('/search/movie', query
+          , 'movies');
+          this.getData('/search/tv', query, 'series');
+        },
+
+        getData(endPoint, query, target) {
           const {baseUri, key, language} = this.api;
           const config = {
             params: {
@@ -39,10 +51,10 @@ export default {
               query,
             },
           };
-          axios.get(baseUri + '/search/movie', config).then((res) => {
-            this.movies = res.data.results
-          })
+          axios.get(`${baseUri}${endPoint}`, config).then((res) => {
+            this[target] = res.data.results;
+          });
         },
     },
-}
+};
 </script>
