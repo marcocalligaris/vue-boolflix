014cfc2aa6fbc400047d8e643366892b6d68e577
marcocalligaris <marcocally@gmail.com>
2022-08-22 16:40:14 +0200

    Added Header css

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        <BaseSearch placeholder="Cerca qui" @search="newSearch" />
+    <div class="py-4" id="header">
+        <div class="container d-flex justify-content-between align-items-center">
+            <a href="#"><h1>BOOLFLIX</h1></a>
+            <BaseSearch placeholder="Cerca qui" @search="newSearch" />
+        </div>
     </div>
 </template>
 
@@ -17,5 +20,19 @@ export default {
     },
 }
 </script>
+<style lang="scss" scoped>
+#header {
+    background-color: black;
+    position: sticky;
+    left: 0;
+    right: 0;
+    top: 0;
+    a {
+        text-decoration: none;
+        color: red;
+        font-size: 1.5rem;
+    }
+}
+</style>
 
 
