63fc3ded476a14a32db6cef93b8a192f51e49f70
marcocalligaris <marcocally@gmail.com>
2022-08-22 11:13:03 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
   <div>
-    <BaseSearch placeholder="Cerca qui" @search="search" />
-    <section id="movies">
-      <h1>MOVIES</h1>
-      <MediaCard v-for="movie in movies" :key="movie.id" :media='movie' />
-
-    </section>
-    <section id="series">
-      <h1>SERIES</h1>
-      <MediaCard v-for="serie in series" :key="serie.id" :media='serie' />
-    </section>
+    <BaseHeader class="container" @newSearch="search" />
+    <main>
+      <section id="movies-gallery">
+        <h1>MOVIES</h1>
+        <MediaCard v-for="movie in movies" :key="movie.id" :media='movie' />
+      </section>
+      <section id="series-gallery">
+        <h1>SERIES</h1>
+        <MediaCard v-for="serie in series" :key="serie.id" :media='serie' />
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import BaseSearch from './components/BaseSearch.vue';
 import axios from 'axios';
 import MediaCard from './components/MediaCard.vue';
+import BaseHeader from './components/BaseHeader.vue';
 export default {
     name: "App",
-    components: { BaseSearch, MediaCard, },
+    components: { MediaCard, BaseHeader },
     data() {
       return {
         movies: [],
         series: [],
+        query: "",
         api: {
           baseUri: 'https://api.themoviedb.org/3',
           key: 'b6a442a03c6d2f2cb8e2c9395b576193',
@@ -36,12 +38,9 @@ export default {
           if(!query) {this.movies = this.series = [];
           return;
           }
-
-          this.getData('/search/movie', query
-          , 'movies');
+          this.getData('/search/movie', query, 'movies');
           this.getData('/search/tv', query, 'series');
         },
-
         getData(endPoint, query, target) {
           const {baseUri, key, language} = this.api;
           const config = {
@@ -58,3 +57,4 @@ export default {
     },
 };
 </script>
+
--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,13 +1,21 @@
 <template>
     <div>
+        <BaseSearch placeholder="Cerca qui" @emitSearch="newSearch" />
     </div>
 </template>
 
-<script>
+<script> 
+import BaseSearch from './BaseSearch.vue';
 
 export default {
     name: "BaseHeader",
-    
+    components: { BaseSearch },
+    methods: {
+        newSearch(value) {
+            this.$emit('search', value);
+        },
+    },
 }
 </script>
+
 
